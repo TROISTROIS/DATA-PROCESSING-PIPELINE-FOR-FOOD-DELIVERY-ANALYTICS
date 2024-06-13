bbb4042802f6fb56cd455eb0307c96db1dd5660f
TROIS <franciscangeno1@gmail.com>
2024-06-13 22:44:43 +0300

    New Cluster

--- pyspark_job.py
+++ pyspark_job.py
@@ -48,7 +48,7 @@ jdbc_url = f"jdbc:redshift://redshift-cluster-spark-load.cyy1gmfmb9hv.us-east-1.
 aws_iam_role = "arn:aws:iam::590183810146:role/redshift-role"
 temp_dir="s3://food-delivery-bucket-fn/temp-folder/"
 target_table = 'food_delivery'
-table_schema = "order_id VARCHAR(20), customer_id VARCHAR(10), restaurant_id VARCHAR(10), order_time timestamp, customer_location VARCHAR(200), restaurant_location VARCHAR(200), order_value double, rating double, delivery_time timestamp"
+table_schema = "order_id string, customer_id string, restaurant_id string, order_time timestamp, customer_location string, restaurant_location string, order_value double, rating double, delivery_time timestamp"
 create_table_query = f"""
     CREATE TABLE IF NOT EXISTS {target_table} ({table_schema})
     USING io.github.spark_redshift_community.spark.redshift
